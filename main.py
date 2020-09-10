8b78ea2376a2eb65175fb7d24542a700348ca0c4
tbsmcd <tbsmcd@gmail.com>
2020-09-11 00:16:55 +0900

    フォーマットもyamlに

--- main.py
+++ main.py
@@ -10,11 +10,13 @@ class PySimpleRss:
     def __init__(self):
         with open('settings.yml') as file:
             self.config = yaml.safe_load(file.read())
+            self.rows = self.config.get('rows', 6)
+            self.length = self.config.get('length', 17)
         self.entries = {}
         self.get_all_entries()
         self.layout = [[
             sg.Column([[
-                sg.T(self.format_text(v), enable_events=True, key='link_' + v['key'])] for v in site
+                sg.T(self.format_text(v, self.length), enable_events=True, key='link_' + v['key'])] for v in site
             ]) for site in self.entries.values()
         ]]
 
@@ -36,10 +38,13 @@ class PySimpleRss:
             self.entries[rss['key']] = entries
 
     def jump_link(self, key):
+        browser = self.config.get('browser_path')
+        if browser is None:
+            return
         for site in self.entries.values():
             for entry in site:
                 if entry['key'] == key:
-                    webbrowser.get(self.config['browser']['path']).open(entry['link'])
+                    webbrowser.get(browser).open(entry['link'])
 
     def view(self):
         window = sg.Window('NEWS', self.layout)
@@ -54,7 +59,7 @@ class PySimpleRss:
                 self.get_all_entries()
                 for site in self.entries.values():
                     for entry in site:
-                        window['link_' + entry['key']].update(self.format_text(entry))
+                        window['link_' + entry['key']].update(self.format_text(entry, self.length))
 
 
 if __name__ == '__main__':
