9c1dc95dc07ff6eb11c2df6f211799eb7f448ac7
tbsmcd <tbsmcd@gmail.com>
2020-09-13 23:28:42 +0900

    命名規則に準拠

--- main.py
+++ main.py
@@ -4,19 +4,19 @@ import yaml
 import webbrowser
 
 
-class Reader:
+class MainWindow:
     def __init__(self):
         with open('settings.yml') as file:
-            self.config = yaml.safe_load(file.read())
-        self.entries = {}
+            self.__config = yaml.safe_load(file.read())
+        self.__entries = {}
 
-    def format_text(self, v):
+    def __format_text(self, v):
         default_len = 17
-        return '[{0}] '.format(v['site']) + v['title'][:self.config.get('length', default_len)] \
-               + ('...' if v['title'][self.config.get('length', default_len):] else '')
+        return '[{0}] '.format(v['site']) + v['title'][:self.__config.get('length', default_len)] \
+               + ('...' if v['title'][self.__config.get('length', default_len):] else '')
 
-    def get_all_entries(self):
-        for rss in self.config.get('feeds'):
+    def __get_all_entries(self):
+        for rss in self.__config.get('feeds'):
             i = 0
             entries = []
             for ent in feedparser.parse(rss['link'])['entries']:
@@ -24,27 +24,27 @@ class Reader:
                     {'site': rss['name'], 'key': rss['key'] + str(i), 'link': ent['link'], 'title': ent['title']}
                 )
                 i += 1
-                if i > self.config.get('rows', 6):
+                if i > self.__config.get('rows', 6):
                     break
-            self.entries[rss['key']] = entries
+            self.__entries[rss['key']] = entries
 
-    def jump_link(self, key: str):
-        browser = self.config.get('browser_path')
+    def __jump_link(self, key: str):
+        browser = self.__config.get('browser_path')
         if browser is None:
             return
-        for site in self.entries.values():
+        for site in self.__entries.values():
             for entry in site:
                 if entry['key'] == key:
                     webbrowser.get(browser).open(entry['link'])
 
     def show(self):
-        sg.theme(self.config.get('theme', 'Dark'))
-        self.get_all_entries()
+        sg.theme(self.__config.get('theme', 'Dark'))
+        self.__get_all_entries()
         layout = [[
             sg.Column([[
-                sg.T(self.format_text(v), enable_events=True, key='link_' + v['key'])] for v in site
-            ]) for site in self.entries.values()
-        ]]
+                sg.T(self.__format_text(v), enable_events=True, key='link_' + v['key'])] for v in site
+            ]) for site in self.__entries.values()
+        ], [sg.B('Setting')]]
         window = sg.Window('Feeds', layout)
         while True:
             event, values = window.read(timeout=60000)
@@ -52,15 +52,15 @@ class Reader:
                 break
             if event.startswith('link_'):
                 key = event.replace('link_', '')
-                self.jump_link(key)
+                self.__jump_link(key)
             elif event in sg.TIMEOUT_KEY:
-                self.get_all_entries()
-                for site in self.entries.values():
+                self.__get_all_entries()
+                for site in self.__entries.values():
                     for entry in site:
-                        window['link_' + entry['key']].update(self.format_text(entry))
+                        window['link_' + entry['key']].update(self.__format_text(entry))
         window.close()
 
 
 if __name__ == '__main__':
-    ps = Reader()
+    ps = MainWindow()
     ps.show()
